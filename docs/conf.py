dd82f399e59505f3e591d1593215887f1a762724
ntiltmann <73590706+ntiltmann@users.noreply.github.com>
2021-08-09 13:24:47 +0200

    readthedocs requirements

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 sys.setrecursionlimit(1500)
 
 
@@ -32,7 +32,7 @@ author = 'ntiltmann'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser','sphinx.ext.autodoc', 'rinoh.frontend.sphinx','sphinx.ext.napoleon']
+extensions = ['myst_parser','sphinx.ext.autodoc','sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
